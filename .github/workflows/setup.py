2dc6b9305eec081f337cd0c17734acd7b19e59ba
Disha Meswania <37867449+dishameswania@users.noreply.github.com>
2025-12-08 18:28:45 +0530

    Update setup.py

--- .github/workflows/setup.py
+++ .github/workflows/setup.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-
-from setuptools import setup
-
 setup(
-    name='jfrog-python-example',
+    name='samplemod',
     version='1.0',
     description='Project example for building Python project with JFrog products',
     author='JFrog',
     author_email='jfrog@jfrog.com',
     url='https://github.com/jfrog/project-examples',
-    packages=['pythonExample'],
+    packages=['sample'],
     install_requires=['PyYAML>3.11', 'nltk'],
 )
